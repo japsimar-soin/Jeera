e37a0e5ea9f0e09893418ab4e86c5a145736aaf7
Japsimar Soin <japsimarsoin2003@gmail.com>
2025-04-21 20:43:29 +0530

    Modified analytics invalidating queries to fetch updates

--- src/features/tasks/api/use-bulk-update-tasks.ts
+++ src/features/tasks/api/use-bulk-update-tasks.ts
@@ -25,6 +25,8 @@ export const useBulkUpdateTasks = () => {
 		},
 		onSuccess: () => {
 			toast.success("Tasks updated");
+			queryClient.invalidateQueries({ queryKey: ["project-analytics"] });
+			queryClient.invalidateQueries({ queryKey: ["workspace-analytics"] });
 			queryClient.invalidateQueries({ queryKey: ["tasks"] });
 		},
 		onError: () => {
