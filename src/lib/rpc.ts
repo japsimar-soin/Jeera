396104a587d948717a9c01e593a43d6b987291ce
Japsimar Soin <japsimarsoin2003@gmail.com>
2025-06-13 20:34:04 +0530

    Updated rpc.ts to support all domains in Vercel prod

--- src/lib/rpc.ts
+++ src/lib/rpc.ts
@@ -2,5 +2,14 @@ import { hc } from "hono/client";
 // import { AppType } from "@/app/api/[[...route]]/route";
 import { AppType } from "@/lib/hono-app";
 
+//------------------Added to support all domains in Vercel (earlier it was supporting the default domain, and not my custom domain)------------
 
-export const client = hc<AppType>(process.env.NEXT_PUBLIC_APP_URL!);
+const baseURL =
+  typeof window !== "undefined"
+    ? window.location.origin
+    : process.env.NEXT_PUBLIC_APP_URL ?? (() => { throw new Error("NEXT_PUBLIC_APP_URL is not set"); })();
+
+export const client = hc<AppType>(baseURL);
+
+
+//export const client = hc<AppType>(process.env.NEXT_PUBLIC_APP_URL!);
