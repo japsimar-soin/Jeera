45e4dcc0fd4b57b7dcf9574ab401509c769f4bd8
Japsimar Soin <japsimarsoin2003@gmail.com>
2025-06-13 20:18:41 +0530

    Changed src/app/api/[[...route]]/route.ts and created a new hono-app file in lib folder and made changes in rpc.ts file to accommodate previous changes made - to make it production ready

--- src/lib/rpc.ts
+++ src/lib/rpc.ts
@@ -1,4 +1,6 @@
 import { hc } from "hono/client";
-import { AppType } from "@/app/api/[[...route]]/route";
+// import { AppType } from "@/app/api/[[...route]]/route";
+import { AppType } from "@/lib/hono-app";
+
 
 export const client = hc<AppType>(process.env.NEXT_PUBLIC_APP_URL!);
